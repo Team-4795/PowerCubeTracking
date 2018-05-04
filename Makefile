6fa886c79c57fddb03c658cada867980333bbfbf
Amr Moussa <amrmoussa100@gmail.com>
2018-05-04 00:11:44 -0400

    Added network tables dependences, better filtering, and a debug toggle

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ LFLAGS = $(shell pkg-config --libs ${LIBS})
 CFLAGS = -std=gnu++11 -g $(shell pkg-config --cflags ${LIBS})
 
 all: build ${BUILD_FILES}
-	g++ -o build/PowerCubeTracking ${BUILD_FILES} ${LFLAGS}
+	g++ -o build/PowerCubeTracking ${BUILD_FILES} ${LFLAGS} -lntcore
 clean:
 	-rm -rf build/
 build/%.o: src/%.cpp
--- src/tracker.cpp
+++ src/tracker.cpp
@@ -1,4 +1,6 @@
 #include <opencv2/opencv.hpp>
+#include <ntcore.h>
+#include <networktables/NetworkTable.h>
 
 using namespace cv;
 
@@ -19,17 +21,17 @@ int main(int argc, char **argv)
 {
 
   int isDebugging = 1;
-  
+
   VideoCapture capture(0);
   Size size;
   Mat rawFrame;
   Mat hsvFrame;
   Mat thresholdFrame;
   Mat contourFrame;
-  
+
   Scalar hsvMin(lowH, lowS, lowV);
   Scalar hsvMax(highH, highS, highV);
-  
+
   //grab frame from the Camera and store its size
   capture >> rawFrame;
   int height = rawFrame.size().height;
@@ -41,56 +43,62 @@ int main(int argc, char **argv)
   contourFrame = Mat(size, CV_8UC1);
 
   namedWindow("contour");
-  
-  if(isDebugging)
+
+  if (isDebugging)
   {
     debugMode();
   }
-  
-  while(true)
+
+  while (true)
   {
     capture >> rawFrame;
-    
-    //Change into HLS colorspace for better filtering
+
     cvtColor(rawFrame, hsvFrame, CV_BGR2HSV);
     inRange(hsvFrame, hsvMin, hsvMax, thresholdFrame);
+
+    //open the image (scale down and then back up) this removes small noise and smaller yellow reflections
+    morphologyEx(thresholdFrame, thresholdFrame, 2, getStructuringElement(MORPH_RECT, Size(10, 10), Point(-1, -1)));
 
     //find contours in the image
     vector<vector<Point>> initContours;
     vector<Vec4i> hierarchy;
-    morphologyEx(thresholdFrame, thresholdFrame, 2, getStructuringElement(MORPH_RECT, Size(10, 10), Point(-1, -1)));
     Canny(thresholdFrame, contourFrame, cannyThresh, cannyThresh * 3, 3);
-    findContours(contourFrame, initContours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
+    findContours(contourFrame, initContours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-    //filter out bad contours to find only the largest one on screen
+    //filter out bad contours to find only the largest one on screen, store its index in latestIndex varible
     int largestSize = -1;
     int latestIndex = -1;
-    
-    for(int i = 0;i < initContours.size(); i++)
+
+    for (int i = 0; i < initContours.size(); i++)
     {
       int size = contourArea(initContours[i]);
-      if(size > largestSize)
+      if (size > largestSize)
       {
-	largestSize = size;
-	latestIndex = i;
+        largestSize = size;
+        latestIndex = i;
       }
     }
 
-    if(latestIndex != -1) 
+    //if we have a contour that satisfies requirements, draw a green outline around it
+    if (latestIndex != -1)
       drawContours(rawFrame, initContours, latestIndex, Scalar(0, 255, 0), 3);
 
-    //update ratios from sliders
+    //update min and max values from sliders
     hsvMin = Scalar(lowH, lowS, lowV);
     hsvMax = Scalar(highH, highS, highV);
 
     //display images
     imshow("raw", rawFrame);
-    imshow("contour", contourFrame);
+    if (isDebugging)
+    {
+      imshow("contour", contourFrame);
+    }
+
     //check if ESC is pressed to exit the program;
     if ((cvWaitKey(10) & 255) == 27)
       break;
   }
-  
+
   return 0;
 }
 
